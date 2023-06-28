9ba1a47b4c9d3fbbf81bec961d689f5741e9ce9d
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-28 18:29:55 +0200

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static string Czas(int czas)
+    {
+        int godziny = czas / 3600;
+        int minuty = (czas % 3600) / 60;
+        int sekundy = czas % 60;
+
+        string sGodziny = godziny.ToString("D2");
+        string sMinuty = minuty.ToString("D2");
+        string sSekundy = sekundy.ToString("D2");
+
+        return $"{sGodziny}:{sMinuty}:{sSekundy}";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Podaj czas w sekundach:");
+        int czas = int.Parse(Console.ReadLine());
+        string sformatowanyCzas = Czas(czas);
+        Console.WriteLine(sformatowanyCzas);
+    }
+}
